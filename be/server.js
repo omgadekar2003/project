54f6355733d77e214c49b4645a9a37b7ca0ceab5
sakshi Kakde <sakshi.kakde2004@gmail.com>
2025-04-03 16:30:46 +0530

    cors

--- be/server.js
+++ be/server.js
@@ -1,16 +1,35 @@
-
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/authRoutes");
-
+// const app = express();
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+// app.options("/*", function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Authorization, Content-Length, X-Requested-With"
+//   );
+//   res.sendStatus(200);
+// });
+
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
+
 app.use("/api/auth", authRoutes);
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
