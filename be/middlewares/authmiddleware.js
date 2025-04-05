34474ac6c887cccc0f0e4e115a2ec3cd3675d733
sakshi Kakde <sakshi.kakde2004@gmail.com>
2025-04-05 16:01:16 +0530

    authmiddleware change

--- be/middlewares/authmiddleware.js
+++ be/middlewares/authmiddleware.js
@@ -1,39 +1,22 @@
-// const jwt = require("jsonwebtoken");
-
-// const authenticateUser = (req, res, next) => {
-//     const token = req.cookies.token;
-//     if (!token) {
-//         return res.status(401).json({ message: "Access denied. No token provided." });
-//     }
-
-//     try {
-//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//         req.user = decoded;
-//         next();
-//     } catch (error) {
-//         res.status(403).json({ message: "Invalid token" });
-//     }
-// };
-
-// module.exports = authenticateUser;
-
-
 const jwt = require("jsonwebtoken");
 
 const authenticateUser = (req, res, next) => {
-    const token = req.cookies.token; // Get token from cookies
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res
+      .status(401)
+      .json({ message: "Access denied. No token provided." });
+  }
 
-    if (!token) {
-        return res.status(401).json({ message: "Access denied. No token provided." });
-    }
+  const token = authHeader.split(" ")[1];
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded; // Attach user data to req object
-        next();
-    } catch (error) {
-        return res.status(403).json({ message: "Invalid token" });
-    }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { userId: decoded.userId };
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token." });
+  }
 };
 
 module.exports = authenticateUser;
