07e8ebbe0c1597a50a0c2fd71c1ede60175b90b3
omgadekar <omgadekar25@gmail.com>
2025-04-05 12:03:39 +0530

    profile update

--- be/middlewares/authmiddleware.js
+++ be/middlewares/authmiddleware.js
@@ -1,17 +1,38 @@
+// const jwt = require("jsonwebtoken");
+
+// const authenticateUser = (req, res, next) => {
+//     const token = req.cookies.token;
+//     if (!token) {
+//         return res.status(401).json({ message: "Access denied. No token provided." });
+//     }
+
+//     try {
+//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//         req.user = decoded;
+//         next();
+//     } catch (error) {
+//         res.status(403).json({ message: "Invalid token" });
+//     }
+// };
+
+// module.exports = authenticateUser;
+
+
 const jwt = require("jsonwebtoken");
 
 const authenticateUser = (req, res, next) => {
-    const token = req.cookies.token;
+    const token = req.cookies.token; // Get token from cookies
+
     if (!token) {
         return res.status(401).json({ message: "Access denied. No token provided." });
     }
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
+        req.user = decoded; // Attach user data to req object
         next();
     } catch (error) {
-        res.status(403).json({ message: "Invalid token" });
+        return res.status(403).json({ message: "Invalid token" });
     }
 };
 
